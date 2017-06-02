071f656eb14c89ce08781097d30da6dc46946cf0
Dinesh Vankayalapati <Dinesh Vankayalapati>
2017-06-02 15:30:04 -0500

    angular2task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {MaterialModule, MdNativeDateModule} from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { DetailComponentComponent } from './detail-component/detail-component.component';
+import { ListComponentComponent } from './list-component/list-component.component';
+import {HolidayServiceService} from './holiday-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponentComponent,
+    ListComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule,MaterialModule,
+    MdNativeDateModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path:'details',component:DetailComponentComponent},
+      {path:'list',component:ListComponentComponent},
+    ])
   ],
-  providers: [],
+  providers: [HolidayServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
